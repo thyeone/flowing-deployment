f13ee75f08ffc00c1f7dc433a2b40d048d244254
thyeone <thyeonee@gmail.com>
2024-01-16 00:13:32 +0900

    feat: Input 컴포넌트 구현 #9

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { cn } from '@/utils/cn';
+import CheckIcon from '@public/svg/check-16.svg';
+import type { UseFormRegisterReturn } from 'react-hook-form';
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> &
+  React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
+    element: 'input' | 'textarea';
+    id: string;
+    label?: string;
+    required?: boolean;
+    isDark?: boolean;
+    register?: UseFormRegisterReturn;
+  };
+
+export default function Input({
+  element,
+  id,
+  label,
+  register,
+  required,
+  isDark,
+  className,
+  ...rest
+}: InputProps) {
+  return (
+    <div className="mx-10">
+      {label && (
+        <div className="mb-4 flex items-center gap-x-1">
+          <label htmlFor={id} className={`font-bold text-[#050505] ${isDark && 'dark:text-white'}`}>
+            {label}
+          </label>
+          {required && <CheckIcon />}
+        </div>
+      )}
+      {element === 'input' ? (
+        <input
+          {...rest}
+          {...register}
+          id={id}
+          className={cn(
+            `focus:border-brand h-[52px] w-full rounded-lg border-[1px] border-[#EAEAEA] bg-transparent px-4 text-sm outline-none transition duration-500`,
+            className,
+            {
+              'dark:border-[#292929] dark:text-white': isDark,
+            },
+          )}
+        />
+      ) : (
+        <textarea
+          {...rest}
+          {...register}
+          id={id}
+          className={cn(
+            `focus:border-brand h-[108px] w-full resize-none rounded-lg border-[1px] border-[#EAEAEA] bg-transparent px-4 pt-4 text-sm outline-none transition duration-500`,
+            className,
+            {
+              'dark:border-[#292929] dark:text-white': isDark,
+            },
+          )}
+        />
+      )}
+    </div>
+  );
+}
