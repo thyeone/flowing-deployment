153bffb50b9c23a700fa8f8ed34218e3f11cf7b0
thyeone <thyeonee@gmail.com>
2024-01-23 17:50:02 +0900

    feat: 버튼 컴포넌트 구현 #11

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,58 +1,29 @@
 'use client';
-import React, { useState } from 'react';
+
 import { cn } from '@/utils/cn';
 
-const buttonColors = {
-  transparent: {
-    default: 'bg-transparent text-[#050505] border-[#DCDBDC] dark:text-brand dark:border-brand',
-    pressed: 'bg-transparent text-[#FF3D7D] border-[#FF3D7D] dark:text-brand dark:border-brand',
-    disabled: 'bg-transparent text-[#C7C7C7] border-[#C7C7C7] dark:text-brand dark:border-brand',
-  },
-  black: {
-    default:
-      'bg-[#050505] text-white border-transparent dark:bg-white dark:text-[#050505] dark:border-transparent',
-    pressed:
-      'bg-[#292929] text-white border-transparent dark:bg-white dark:text-[#050505] dark:border-transparent',
-    disabled:
-      'bg-[#DCDBDC] text-white border-transparent dark:bg-white dark:text-[#050505] dark:border-transparent',
-  },
-};
-
 type ButtonProps = {
-  color: keyof typeof buttonColors;
-  children: React.ReactNode;
-  size?: 'full' | 'half';
+  isDark?: boolean;
   disabled?: boolean;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function Button({ color, children, size, disabled, ...rest }: ButtonProps) {
-  const [isPressed, setIsPressed] = useState(false);
-
-  const handleClick = () => {
-    if (!disabled) {
-      setIsPressed(true);
-
-      setTimeout(() => {
-        setIsPressed(false);
-      }, 200);
-    }
-  };
-
+export default function Button({
+  isDark,
+  disabled,
+  className,
+  children,
+  ...rest
+}: PropsWithStrictChildren<ButtonProps>) {
   return (
     <button
       {...rest}
-      onClick={handleClick}
       className={cn(
-        'flex items-center justify-center rounded-lg border border-solid px-4 py-2',
-        'text-16 font-weight-500 leading-16 text-center',
+        `flex h-[52px] w-full items-center justify-center rounded-lg bg-gray-900 font-medium text-white`,
         {
-          [buttonColors[color].default]: !disabled && !isPressed,
-          [buttonColors[color].pressed]: !disabled && isPressed,
-          [buttonColors[color].disabled]: disabled,
+          'dark:bg-white dark:text-black': isDark,
+          'bg-gray-200': disabled,
         },
-        size === 'full' ? 'w-full' : '',
-        size === 'half' ? 'w-1/2' : '',
-        rest.className,
+        className,
       )}
       disabled={disabled}
     >
