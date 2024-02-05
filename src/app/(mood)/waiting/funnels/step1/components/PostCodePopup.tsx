30384c7c69a50eb7476c445b6dc89d36d88256d9
thyeone <thyeonee@gmail.com>
2024-02-05 23:32:10 +0900

    refactor: Popup에서만 사용되는 Header 분리 #30

---
+++ src/app/(mood)/waiting/funnels/step1/components/PostCodePopup.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { Portal } from '@/components/common/Portal';
+import DaumPostcodeEmbed, { type Address } from 'react-daum-postcode';
+import type { MoodContextValue } from '../../../components/MoodContext';
+import { type UseFormReturn } from 'react-hook-form';
+import PopupHeader from '../../../components/PopupHeader';
+
+type PostCodePopupProps = {
+  useForm: UseFormReturn<MoodContextValue>;
+  onClose: VoidFunction;
+};
+
+export default function PostCodePopup({ useForm, onClose }: PostCodePopupProps) {
+  const { setValue } = useForm;
+
+  const handleComplete = async (data: Address) => {
+    const { address, sido, zonecode, bname, sigungu } = data;
+    setValue(
+      'address',
+      {
+        roadAddress: address,
+        zonecode,
+        sido,
+        sigungu,
+        bname,
+      },
+      {
+        shouldDirty: true,
+      },
+    );
+    onClose();
+  };
+
+  return (
+    <Portal>
+      <PopupHeader text="거주 지역" onClose={onClose} />
+      <DaumPostcodeEmbed
+        className="fixed inset-x-0 z-30 mx-auto mt-14 min-h-screen max-w-[430px]"
+        onComplete={handleComplete}
+        shorthand={false}
+        autoClose={false}
+      />
+    </Portal>
+  );
+}
