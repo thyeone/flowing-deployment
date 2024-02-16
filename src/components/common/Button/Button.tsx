cc56151ad2d63bfaeae53db4fef2417a8b42d3c6
thyeone <thyeonee@gmail.com>
2024-02-17 00:38:08 +0900

    refactor: 불필요한 코드 삭제 #23

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { cn } from '@/utils/cn';
-import Spacing from '../Spacing';
+import { ButtonWrapper } from '.';
 
 type ButtonProps = {
   isDark?: boolean;
@@ -16,22 +16,20 @@ export default function Button({
   ...rest
 }: PropsWithStrictChildren<ButtonProps>) {
   return (
-    <>
-      <button
-        {...rest}
-        className={cn(
-          `flex h-[52px] w-full items-center justify-center rounded-lg bg-gray-900 px-4 font-medium text-white`,
-          {
-            'dark:bg-white dark:text-gray-900': isDark,
-            'bg-gray-200': disabled,
-            'dark:bg-gray-700 dark:text-gray-500': disabled && isDark,
-          },
-          className,
-        )}
-        disabled={disabled}
-      >
-        {children}
-      </button>
-    </>
+    <button
+      {...rest}
+      className={cn(
+        `flex h-[52px] w-full items-center justify-center rounded-lg bg-gray-900 px-4 font-medium text-white`,
+        {
+          'dark:bg-white dark:text-gray-900': isDark,
+          'bg-gray-200': disabled,
+          'dark:bg-gray-700 dark:text-gray-500': disabled && isDark,
+        },
+        className,
+      )}
+      disabled={disabled}
+    >
+      {children}
+    </button>
   );
 }
