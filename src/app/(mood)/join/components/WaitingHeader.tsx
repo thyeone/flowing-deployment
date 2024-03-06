f258cc465c60d78af355223b76dbd0f8424fec78
thyeone <thyeonee@gmail.com>
2024-03-07 02:34:55 +0900

    refactor: 폴더위치 변경 #54

--- src/app/(mood)/join/components/WaitingHeader.tsx
+++ src/app/(mood)/join/components/WaitingHeader.tsx
@@ -1,5 +1,5 @@
 import { Header } from '@/components/Header';
-import BackButton from '../../../components/BackButton';
+import BackButton from './BackButton';
 import { cn } from '@/utils';
 
 type WaitingHeaderProps = {
