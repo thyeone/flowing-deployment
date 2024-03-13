2d87accce0b2f2602c01b9b894c69de6ac8bcb24
thyeone <thyeonee@gmail.com>
2024-03-13 21:03:00 +0900

    feat: member_id를 통해 알맞는 페이지로 매핑한다 #71

--- src/app/(sub)/auth/kakao/page.tsx
+++ src/app/(sub)/auth/kakao/page.tsx
@@ -1,23 +1,38 @@
 'use client';
 
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useGetLogin } from '@/apis/auth';
+import { useGetMember } from '@/apis/member';
 import { BASE_DOMAIN } from '@/constants/environment';
-import { setToken } from '@/utils';
+import { decodeAccessToken, setToken } from '@/utils';
 
 export default function KakaoCallbackPage() {
   const router = useRouter();
   const params = useSearchParams();
   const code = params.get('code') || '';
+  const [memberId, setMemberId] = useState(decodeAccessToken() || '');
   const { data } = useGetLogin(code, 'kakao', `${BASE_DOMAIN}/auth/kakao`);
+
+  const { data: profile } = useGetMember(memberId);
 
   useEffect(() => {
     if (data) {
       const { accessToken, refreshToken } = data;
       setToken(accessToken, refreshToken);
-      router.replace('/join');
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (profile?.status === 'IN_SING_UP') router.replace('/join');
+
+    if (profile?.status === 'ACTIVE') router.replace('/home');
+  }, [profile]);
+
+  useEffect(() => {
+    if (decodeAccessToken()) {
+      setMemberId(decodeAccessToken()!);
     }
   }, [data]);
 
