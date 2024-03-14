c0f163098a0456e021e0ee6b5548db501410c2aa
thyeone <thyeonee@gmail.com>
2024-03-14 19:34:04 +0900

    feat: 이미지 압축 유틸함수 생성 #82

---
+++ src/utils/compressImage.ts
@@ -0,0 +1,11 @@
+import imageCompression from 'browser-image-compression';
+
+export const compressImage = async (file: File) => {
+  const compressedImage = await imageCompression(file, {
+    maxSizeMB: 4,
+    maxWidthOrHeight: 1920,
+    useWebWorker: true,
+  });
+
+  return new File([compressedImage], file.name, { type: file.type });
+};
