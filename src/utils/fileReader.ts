4552f8d6b607453ea72b424f89b91454a7200b03
thyeone <thyeonee@gmail.com>
2024-03-14 19:34:55 +0900

    refactor: 사용하지 않는 파일 제거 #82

--- src/utils/fileReader.ts
+++
@@ -1,9 +0,0 @@
-export function convertFileToBase64(file: File) {
-  const reader = new FileReader();
-  reader.readAsDataURL(file);
-  return new Promise((resolve) => {
-    reader.onload = () => {
-      resolve(reader.result);
-    };
-  });
-}
