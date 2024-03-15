5aa8d979d54a57ac166867be2801c499215a72c1
thyeone <thyeonee@gmail.com>
2024-03-15 19:29:44 +0900

    refactor: type을 옵셔널로 받도록 수정 #83

--- src/hooks/useToast.tsx
+++ src/hooks/useToast.tsx
@@ -3,16 +3,22 @@ import { Toast } from '@/components/Overlay';
 import { useOverlay } from '.';
 
 const useToast = () => {
-  const { open, close, exit } = useOverlay({ delay: 1500 });
+  const { open } = useOverlay({ delay: 1500 });
 
-  const openToast = ({ message, type }: { message: string; type: 'warning' | 'default' }) =>
+  const openToast = ({
+    message,
+    type = 'default',
+  }: {
+    message: string;
+    type?: 'warning' | 'default';
+  }) =>
     open(({ isOpen }) => (
       <Toast isOpen={isOpen} type={type}>
         {message}
       </Toast>
     ));
 
-  return { openToast, close, exit };
+  return { openToast };
 };
 
 export default useToast;
