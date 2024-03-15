efa19f521bcbf0dfc417802a277ca975761c5e3a
thyeone <thyeonee@gmail.com>
2024-03-15 19:11:34 +0900

    feat: 토스트 구현 #83

---
+++ src/components/Modal/Toast.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import WarningIcon from '@public/svg/warning.svg';
+import { motion } from 'framer-motion';
+
+import { fadeInOut } from '@/constants';
+import { cn } from '@/utils';
+
+import { AnimatePortal } from '.';
+
+type ToastProps = {
+  isOpen: boolean;
+  type?: 'warning' | 'default';
+};
+
+export default function Toast({
+  children,
+  isOpen,
+  type = 'default',
+}: PropsWithStrictChildren<ToastProps>) {
+  return (
+    <AnimatePortal isOpen={isOpen}>
+      <motion.div
+        {...fadeInOut}
+        className="fixed inset-x-0 top-4 z-modal mx-auto flex justify-center px-5"
+      >
+        <div
+          className={cn(
+            `inline-flex h-10 w-full max-w-[430px] items-center gap-x-1 rounded-xl bg-[#212123] px-4 py-3 text-sm text-white opacity-80`,
+            {
+              'bg-[#FF3333]': type === 'warning',
+            },
+          )}
+        >
+          {type === 'warning' && <WarningIcon />}
+          {children}
+        </div>
+      </motion.div>
+    </AnimatePortal>
+  );
+}
---
+++ src/hooks/useToast.tsx
@@ -0,0 +1,18 @@
+import { Toast } from '@/components/Modal';
+
+import { useOverlay } from '.';
+
+const useToast = () => {
+  const { open, close, exit } = useOverlay({ delay: 1500 });
+
+  const openToast = ({ message, type }: { message: string; type: 'warning' | 'default' }) =>
+    open(({ isOpen }) => (
+      <Toast isOpen={isOpen} type={type}>
+        {message}
+      </Toast>
+    ));
+
+  return { openToast, close, exit };
+};
+
+export default useToast;
