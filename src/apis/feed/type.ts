98be27d4cdb689478807ac0032fe4d3ad97930df
saseungg <yeon14766@gmail.com>
2024-03-23 07:01:23 +0900

    refactor: feed 응답 타입에서 contents 타입 분리 #90

--- src/apis/feed/type.ts
+++ src/apis/feed/type.ts
@@ -1,17 +1,19 @@
+export type Contents = {
+  profilePic: string;
+  nickname: string;
+  age: number;
+  region: string;
+  content: string;
+  channel: string;
+  viewCount: number;
+  likeCount: number;
+  commentCount: number;
+  createdAt: string;
+  updateAt: string;
+};
+
 export type FeedResponse = {
   id: number;
-  contents: {
-    profilePic: string;
-    nickname: string;
-    age: number;
-    region: string;
-    content: string;
-    channel: string;
-    viewCount: number;
-    likeCount: number;
-    commentCount: number;
-    createdAt: string;
-    updateAt: string;
-  };
+  contents: Contents;
   images: string[];
 };
