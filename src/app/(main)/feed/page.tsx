6b606f19d208b1f5a2b935673f09b70b818dc29e
saseungg <yeon14766@gmail.com>
2024-03-23 04:08:58 +0900

    feat: 페이지 feed 목록 조회 api 연동 #90

--- src/app/(main)/feed/page.tsx
+++ src/app/(main)/feed/page.tsx
@@ -1,13 +1,32 @@
+'use client';
+
+import { useGetFeed } from '@/apis/feed';
 import { Header } from '@/components/Header';
 import BottomTabBar from '@/components/TabBar/BottomTabBar';
 
 export default function Feed() {
+  const { data: feed } = useGetFeed('');
+
   return (
     <div>
       <Header>
         <Header.FeedHeader>피드</Header.FeedHeader>
       </Header>
-      <h1>Feed</h1>
+      <span>왜 안뜨노</span>
+      {feed?.map(({ id, contents, images }) => (
+        <li key={id}>
+          <p>{contents.channel}</p>
+          <img src={contents.profilePic} />
+          <p>{contents.nickname}.</p>
+          <p>{contents.age}</p>
+          <p>{contents.region}</p>
+          <p>{contents.createdAt}</p>
+          <p>{contents.content}</p>
+          <p>{contents.likeCount}</p>
+          <p>{contents.viewCount}</p>
+          <p>{contents.commentCount}</p>
+        </li>
+      ))}
       <BottomTabBar />
     </div>
   );
