16d0a3652ae5d988fbf71d59b61684863dd40a5a
saseungg <yeon14766@gmail.com>
2024-03-23 14:02:28 +0900

    feat: feedItem 컴포넌트 분리 페이지 적용 #90

--- src/app/(main)/feed/page.tsx
+++ src/app/(main)/feed/page.tsx
@@ -4,6 +4,8 @@ import { useGetFeed } from '@/apis/feed';
 import { Header } from '@/components/Header';
 import BottomTabBar from '@/components/TabBar/BottomTabBar';
 
+import FeedItem from './components/FeedItem';
+
 export default function Feed() {
   const { data: feed } = useGetFeed('');
 
@@ -12,21 +14,7 @@ export default function Feed() {
       <Header>
         <Header.FeedHeader>피드</Header.FeedHeader>
       </Header>
-      <span>왜 안뜨노</span>
-      {feed?.map(({ id, contents, images }) => (
-        <li key={id}>
-          <p>{contents.channel}</p>
-          <img src={contents.profilePic} />
-          <p>{contents.nickname}.</p>
-          <p>{contents.age}</p>
-          <p>{contents.region}</p>
-          <p>{contents.createdAt}</p>
-          <p>{contents.content}</p>
-          <p>{contents.likeCount}</p>
-          <p>{contents.viewCount}</p>
-          <p>{contents.commentCount}</p>
-        </li>
-      ))}
+      {feed?.map(({ id, contents, images }) => <FeedItem key={id} contents={contents} />)}
       <BottomTabBar />
     </div>
   );
