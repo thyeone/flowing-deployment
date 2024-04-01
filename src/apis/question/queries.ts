77d7a677398344983b09c4ac00141d071d664b83
thyeone <thyeonee@gmail.com>
2024-04-01 18:33:22 +0900

    fix: useQuery로 변경 #97

--- src/apis/question/queries.ts
+++ src/apis/question/queries.ts
@@ -1,9 +1,9 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
 import { type Value, queryKeys, questionApi } from '.';
 
 export const useGetValueQuestion = (type: Value) => {
-  return useSuspenseQuery({
+  return useQuery({
     queryKey: queryKeys.getValue(type),
     queryFn: () => questionApi.getValue(type),
     staleTime: 1000 * 60 * 5,
