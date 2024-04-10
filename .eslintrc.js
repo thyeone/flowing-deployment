354deef6a067bb02ee083e8850a25dbb92f00c3b
thyeone <thyeonee@gmail.com>
2024-04-10 22:43:17 +0900

    chore: lint 설정 추가 #102

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,7 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-extra-boolean-cast': 'off',
+    'prefer-const': 'off',
   },
 };
