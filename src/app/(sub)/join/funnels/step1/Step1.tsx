61872d54ab73da108606277c525302bc574499e3
thyeone <thyeonee@gmail.com>
2024-04-21 18:08:09 +0900

    feat: 모션 교체 #105

--- src/app/(sub)/join/funnels/step1/Step1.tsx
+++ src/app/(sub)/join/funnels/step1/Step1.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import Step1Image from '@public/svg/join-step1.svg';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 import { type MemberResponse, useGetMember } from '@/apis/member';
 import { Button, ButtonWrapper } from '@/components/Button';
 import Spacing from '@/components/Spacing';
+import Video from '@/components/Video';
 import { decodeAccessToken } from '@/utils';
 import { getEmptyProfile } from '@/utils/getEmptyProfile';
 
@@ -18,25 +19,26 @@ export default function Step1({
 }: Pick<ReturnType<typeof useFunnelContext>, 'nextStep' | 'setStep'>) {
   const router = useRouter();
   const memberId = decodeAccessToken() || '';
-  const { data: profile } = useGetMember(memberId);
+  // const { data: profile } = useGetMember(memberId);
 
-  useEffect(() => {
-    if (profile.status === 'ACTIVE') router.replace('/home');
+  // useEffect(() => {
+  //   if (profile.status === 'ACTIVE') router.replace('/home');
 
-    if (profile.status === 'INACTIVE') router.replace('/');
+  //   if (profile.status === 'INACTIVE') router.replace('/');
 
-    if (profile.status === 'IN_SING_UP') {
-      setStep(getEmptyProfile(profile?.profile as MemberResponse['profile']));
-    }
-  }, [profile]);
+  //   if (profile.status === 'IN_SING_UP') {
+  //     setStep(getEmptyProfile(profile?.profile as MemberResponse['profile']));
+  //   }
+  // }, [profile]);
 
-  if (!profile) return null;
+  // if (!profile) return null;
 
   return (
     <>
-      <div className="flex h-[85%] flex-col items-center justify-center">
-        <Step1Image />
-        <Spacing size={28} />
+      <div className="flex h-[80%] flex-col items-center justify-center">
+        <div className="relative h-[300px] w-full overflow-hidden">
+          <Video src="/video/step1-girl.mp4" className="absolute size-full" loop={false} />
+        </div>
         <h1 className="whitespace-pre-wrap text-center text-[22px] font-bold">{`반가워요!\n프로필을 작성하러 가볼까요?`}</h1>
         <p className="mt-3 text-sm text-gray-500">상세 프로필 작성하고 플로잉을 시작해보세요</p>
       </div>
