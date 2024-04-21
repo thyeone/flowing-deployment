4cc19a95ca9fe927d72ca49818c77a5f64e3c223
thyeone <thyeonee@gmail.com>
2024-04-21 18:10:02 +0900

    refactor: useSuspenseQuery로 교체 #105

--- src/apis/question/queries.ts
+++ src/apis/question/queries.ts
@@ -1,9 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
 import { type Value, queryKeys, questionApi } from '.';
 
 export const useGetValueQuestion = (type: Value) => {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: queryKeys.getValue(type),
     queryFn: () => questionApi.getValue(type),
     staleTime: 1000 * 60 * 5,
