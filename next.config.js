8eaeebf6ef7ce2ff43a36572d4452dfa1bb29bae
saseungg <yeon14766@gmail.com>
2024-04-24 17:11:51 +0900

    feat: svg import시 ?url 붙이면 Image 태그에 url로 관리할 수 있도록 처리
    - url을 붙이지 않으면 리액트 컴포넌트로 관리, url을 붙일 경우 url로 처리할 수 있도록 처리하였습니다.
    - svgr 타입 설정

--- next.config.js
+++ next.config.js
@@ -9,10 +9,23 @@ const nextConfig = {
     ],
   },
   webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ['@svgr/webpack'],
+      },
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
   },
