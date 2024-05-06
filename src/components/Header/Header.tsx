cc1efce6f7d775497a1adf1df106a1255e44dfd6
thyeone <thyeonee@gmail.com>
2024-05-06 16:03:33 +0900

    refactor: 불필요한 컴포넌트 제거 #111

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -44,22 +44,6 @@ function Right({ children, className }: PropsWithChildren<{ className?: string }
   return <div className={cn('ml-auto flex items-center', className)}>{children}</div>;
 }
 
-function TextHeader({ children }: PropsWithStrictChildren) {
-  return <span className="text-lg font-bold text-[#212529]">{children}</span>;
-}
-
-function FeedHeader({ children }: PropsWithStrictChildren) {
-  return (
-    <>
-      <BackButton />
-      <span className="font-bold text-[#292929]">{children}</span>
-    </>
-  );
-}
-
 Header.Left = Left;
 Header.Center = Center;
 Header.Right = Right;
-
-Header.TextHeader = TextHeader;
-Header.FeedHeader = FeedHeader;
