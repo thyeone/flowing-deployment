0fd6248b1f538de6f0137319a7a5c285c40a674a
thyeone <thyeonee@gmail.com>
2024-05-06 16:14:25 +0900

    refactor: mbti 별칭 가져오는 함수 분리 #111

--- src/app/(sub)/join/funnels/step2/components/MbtiSection.tsx
+++ src/app/(sub)/join/funnels/step2/components/MbtiSection.tsx
@@ -4,6 +4,7 @@ import { useWatch } from 'react-hook-form';
 
 import RightArrow from '@/assets/RightArrow';
 import { useOverlay } from '@/hooks';
+import { getMbtiAlias } from '@/utils';
 
 import { useJoin1Context } from '../../../components/Join1Context';
 import MbtiBottomSheet from './MbtiBottomSheet';
@@ -35,43 +36,4 @@ export default function MbtiSection() {
       </button>
     </>
   );
-}
-
-function getMbtiAlias(mbti: string) {
-  switch (mbti) {
-    case 'ENTJ':
-      return 'ENTJ - 통솔자';
-    case 'ENTP':
-      return 'ENTP - 변론가';
-    case 'INTJ':
-      return 'INTJ - 전략가';
-    case 'INTP':
-      return 'INTP - 논리술사';
-    case 'INFJ':
-      return 'INFJ - 옹호자';
-    case 'INFP':
-      return 'INFP - 중재자';
-    case 'ENFJ':
-      return 'ENFJ - 선도자';
-    case 'ENFP':
-      return 'ENFP - 활동가';
-    case 'ISTJ':
-      return 'ISTJ - 현실주의자';
-    case 'ISFJ':
-      return 'ISFJ - 수호자';
-    case 'ESTJ':
-      return 'ESTJ - 경영자';
-    case 'ESFJ':
-      return 'ESFJ - 집정관';
-    case 'ISFP':
-      return 'ISFP - 모험가';
-    case 'ISTP':
-      return 'ISTP - 장인';
-    case 'ESFP':
-      return 'ESFP - 연예인';
-    case 'ESTP':
-      return 'ESTP - 사업가';
-    default:
-      return false;
-  }
 }
