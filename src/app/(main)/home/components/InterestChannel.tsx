b2f91c3ab8b3d4115603c6a8cc9f84cd4674ed9c
saseungg <yeon14766@gmail.com>
2024-06-02 14:27:15 +0900

    feat: 관심 채널 구현, 페이지 반영 #113

---
+++ src/app/(main)/home/components/InterestChannel.tsx
@@ -0,0 +1,10 @@
+import ChannelList from './ChannelList';
+
+export default function InterestChannel() {
+  return (
+    <div className="px-5">
+      <h2 className="mb-3 text-lg font-bold">관심 채널</h2>
+      <ChannelList />
+    </div>
+  );
+}
--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,12 +1,14 @@
 import BottomTabBar from '@/components/TabBar/BottomTabBar';
 
 import HomeHeader from './components/HomeHeader';
+import InterestChannel from './components/InterestChannel';
 
 export default function Home() {
   return (
-    <div>
+    <div className="mb-8">
       <HomeHeader />
       <h1>Home</h1>
+      <InterestChannel />
       <BottomTabBar />
     </div>
   );
