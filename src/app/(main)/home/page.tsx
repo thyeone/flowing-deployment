12fc96de84cf2c1ebd14c17da00d729327b4314e
saseungg <yeon14766@gmail.com>
2024-06-02 19:00:17 +0900

    feat: 배너 컴포넌트 구현 #113

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,13 +1,14 @@
 import BottomTabBar from '@/components/TabBar/BottomTabBar';
 
+import Banner from './components/Banner';
 import HomeHeader from './components/HomeHeader';
 import InterestChannel from './components/InterestChannel';
 
 export default function Home() {
   return (
-    <div className="mb-8">
+    <div className="mb-8 px-5">
       <HomeHeader />
-      <h1>Home</h1>
+      <Banner />
       <InterestChannel />
       <BottomTabBar />
     </div>
