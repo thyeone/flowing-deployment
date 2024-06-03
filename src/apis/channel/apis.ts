c108bc4a04048a52a6eb2979e477e9af3c4ab0ef
saseungg <yeon14766@gmail.com>
2024-06-03 20:48:42 +0900

    remove: 채널 관련 쿼리 삭제 #113
    - api 수정되면 다시 구현하고 현재는 사용하지 않으므로 삭제 처리

--- src/apis/channel/apis.ts
+++
@@ -1,6 +0,0 @@
-import http from '../config/instance';
-import type { ChannelResponse } from './type';
-
-export const ChannelApi = {
-  getChannels: async () => await http.get<ChannelResponse[]>('/channels'),
-};
--- src/apis/channel/type.ts
+++
@@ -1,8 +0,0 @@
-export type ChannelResponse = {
-  id: number;
-  name: string;
-  title: string;
-  subTitle: string;
-  image: string;
-  path: string;
-};
