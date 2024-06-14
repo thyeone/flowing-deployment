928f60274c89382d78601c298f95de1850599255
saseungg <yeon14766@gmail.com>
2024-06-15 04:14:36 +0900

    chore: backgroundImage config 추가 #113
    - gradient 관련

---
+++ src/styles/backgroundImage.ts
@@ -0,0 +1,4 @@
+export const backgroundImage = {
+  'gradient-to-b': 'linear-gradient(to bottom, transparent, white 100%)',
+  'gradient-to-t': 'linear-gradient(to top, black 100%, transparent)',
+};
