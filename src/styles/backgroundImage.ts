9cddac6869342a9a4a32afa2190208e9140f5c3e
saseungg <yeon14766@gmail.com>
2024-06-15 21:40:31 +0900

    refactor: gradient 스타일 수정 및 네이밍 수정 #113

--- src/styles/backgroundImage.ts
+++ src/styles/backgroundImage.ts
@@ -1,4 +1,4 @@
 export const backgroundImage = {
-  'gradient-to-b': 'linear-gradient(to bottom, transparent, white 100%)',
-  'gradient-to-t': 'linear-gradient(to top, black 100%, transparent)',
+  'gradient-to-b-light': 'linear-gradient(to bottom, transparent 38%, #FFFCFE 100%)',
+  'gradient-to-b-dark': 'linear-gradient(to bottom, black 71%, #FAFAFD 100%)',
 };
