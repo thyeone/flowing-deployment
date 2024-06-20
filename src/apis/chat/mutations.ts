903a4ff9fd52cd52a089622fe652ec5c360980f0
thyeone <thyeonee@gmail.com>
2024-06-20 18:52:53 +0900

    feat: mutation시 토스트 호출 #109

--- src/apis/chat/mutations.ts
+++ src/apis/chat/mutations.ts
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 
 import { useToast } from '@/hooks';
 
@@ -7,6 +8,7 @@ import { queryKeys } from './keys';
 
 export const usePostChatRequest = () => {
   const { openToast } = useToast();
+
   return useMutation({
     mutationFn: chatApi.postChatRequest,
     onSuccess: () => openToast({ message: '대화 신청에 성공했어요.' }),
@@ -15,12 +17,17 @@ export const usePostChatRequest = () => {
 
 export const usePostChatType = () => {
   const queryClient = useQueryClient();
+  const router = useRouter();
+  const { openToast } = useToast();
 
   return useMutation({
     mutationFn: chatApi.postChatType,
-    onSuccess: () => {
+    onSuccess: (_, { conversationType }) => {
       queryClient.invalidateQueries({ queryKey: queryKeys.getReceiveChat() });
       queryClient.invalidateQueries({ queryKey: queryKeys.getRequestChat() });
+      conversationType === 'ACCEPT'
+        ? openToast({ message: '대화 신청을 승인했어요' })
+        : openToast({ message: '대화 신청을 거절했어요' });
     },
   });
 };
