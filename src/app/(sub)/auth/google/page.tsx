d2760a0be98a0a6834aa27b8d165cd07e1699708
thyeone <thyeonee@gmail.com>
2024-06-20 17:39:26 +0900

    refactor: 구글 로그인 페이지 리펙토링 #109

--- src/app/(sub)/auth/google/page.tsx
+++ src/app/(sub)/auth/google/page.tsx
@@ -1,35 +1,25 @@
 'use client';
 
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import { useGetLogin } from '@/apis/auth';
-import { useGetMember } from '@/apis/member';
 import { BASE_DOMAIN } from '@/constants/environment';
-import { decodeAccessToken, setToken } from '@/utils';
+import { setToken } from '@/utils';
 
 export default function GoogleCallbackPage() {
   const router = useRouter();
   const params = useSearchParams();
   const code = params.get('code') || '';
-  const [memberId, setMemberId] = useState(decodeAccessToken() || '');
   const { data } = useGetLogin(code, 'google', `${BASE_DOMAIN}/auth/google`);
-
-  const { data: profile } = useGetMember(memberId!);
 
   useEffect(() => {
     if (data) {
       const { accessToken, refreshToken } = data;
       setToken(accessToken, refreshToken);
-      setMemberId(decodeAccessToken());
+      router.replace('/join');
     }
   }, [data]);
-
-  useEffect(() => {
-    if (profile?.status === 'IN_SING_UP') router.replace('/join');
-
-    if (profile?.status === 'ACTIVE') router.replace('/home');
-  }, [profile]);
 
   return <></>;
 }
