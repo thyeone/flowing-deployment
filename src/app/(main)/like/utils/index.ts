2636a1268d690ea98880c892e304a19e7add809c
thyeone <thyeonee@gmail.com>
2024-07-01 19:33:48 +0900

    refactor: 사용하지 않는 파일 삭제 #120

--- src/app/(main)/like/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export const getDday = (endDay: string) => {
-  const dday = Math.floor(
-    (new Date(endDay).getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24),
-  );
-  return dday > 0 && dday;
-};
