6bba3f46d86abeba23950c584e8a670e5a243be3
Woojin Lee <nijoow1127@gmail.com>
2024-08-27 00:01:40 +0900

    feat: Divider size option 추가

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@/utils';
 
 type DividerProps = {
-  size?: 'sm' | 'md';
+  size?: 'xs' | 'sm' | 'md';
   isDark?: boolean;
 };
 
@@ -13,6 +13,7 @@ export default function Divider({ size = 'md', isDark }: DividerProps) {
   return (
     <div
       className={cn('absolute inset-x-0 w-full bg-gray-100', {
+        'h-px': size === 'xs',
         'h-1': size === 'sm',
         'h-2': size === 'md',
         'dark:bg-gray-800': isDark,
