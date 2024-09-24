5a073fbc5f5e9eb460b348d3e544b8e6beff0547
thyeone <thyeonee@gmail.com>
2024-09-24 22:48:30 +0900

    feat: LazyMotionProvider 추가 #130

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import FramerLazyMotionProvider from './FramerLazyMotionProvider';
 import NextThemeProvider from './NextThemeProvider';
 import OverlayProvider from './OverlayProvider';
 import QueryProvider from './QueryProvider';
@@ -7,12 +8,14 @@ import ToastProvider from './ToastProvider';
 
 export default function Providers({ children }: PropsWithStrictChildren) {
   return (
-    <ToastProvider>
-      <QueryProvider>
-        <OverlayProvider>
-          <NextThemeProvider>{children}</NextThemeProvider>
-        </OverlayProvider>
-      </QueryProvider>
-    </ToastProvider>
+    <FramerLazyMotionProvider>
+      <ToastProvider>
+        <QueryProvider>
+          <OverlayProvider>
+            <NextThemeProvider>{children}</NextThemeProvider>
+          </OverlayProvider>
+        </QueryProvider>
+      </ToastProvider>
+    </FramerLazyMotionProvider>
   );
 }
