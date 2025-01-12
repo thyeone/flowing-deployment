8a2f628d880f097d2007efb372431709bcd215fd
thyeone <thyeonee@gmail.com>
2025-01-12 00:38:40 +0000

    fix: 인터셉터 로직 미들웨어로

--- src/apis/config/instance.ts
+++ src/apis/config/instance.ts
@@ -1,11 +1,10 @@
 import type { Axios, AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 import axios from 'axios';
+import { getCookie as clientCookie } from 'cookies-next';
 import qs from 'qs';
 
-import { getCookie, logoutAction, setCookie } from '@/actions/cookie';
 import { TOKEN_KEYS } from '@/constants';
 
-import { authApi } from '../auth';
 import { getResponseFromBody } from './common';
 import type { ErrorResponse } from './type';
 
@@ -17,7 +16,7 @@ export const instance = axios.create({
 
 instance.interceptors.request.use(
   async (config: InternalAxiosRequestConfig<any>) => {
-    const accessToken = await getCookie(TOKEN_KEYS.accessToken);
+    const accessToken = clientCookie(TOKEN_KEYS.accessToken);
     if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
 
     return config;
@@ -33,44 +32,6 @@ instance.interceptors.response.use(
   },
   async (error: AxiosError<ErrorResponse, InternalAxiosRequestConfig>) => {
     if (!error.response || !error.config) return Promise.reject(error);
-
-    const {
-      config: originalRequest,
-      response: { data, status },
-    } = error;
-
-    const { code } = data;
-
-    if (status === 401 && code === 'UNAUTHORIZED') {
-      const __accessToken = await getCookie(TOKEN_KEYS.accessToken);
-      const __refreshToken = await getCookie(TOKEN_KEYS.refreshToken);
-
-      if (!__accessToken || !__refreshToken) {
-        window.location.href = '/';
-      }
-
-      if (__accessToken && __refreshToken) {
-        try {
-          const { accessToken, refreshToken } = await authApi.postRefresh(
-            __accessToken,
-            __refreshToken,
-          );
-
-          if (accessToken && refreshToken) {
-            setCookie(TOKEN_KEYS.accessToken, accessToken);
-            setCookie(TOKEN_KEYS.refreshToken, refreshToken);
-          }
-
-          originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
-
-          return instance.request(originalRequest);
-        } catch (error) {
-          await logoutAction();
-        }
-      }
-    } else {
-      window.location.href = '/';
-    }
 
     return Promise.reject(error);
   },
--- src/utils/token.ts
+++ src/utils/token.ts
@@ -3,6 +3,10 @@ import { jwtDecode } from 'jwt-decode';
 
 import { TOKEN_KEYS } from '@/constants';
 
+type JWTPayload = {
+  exp: number;
+};
+
 const REFRESH_EXPIRE_TIME = 7 * 24 * 60 * 60 * 1000;
 // const ACCESS_EXPIRE_TIME = 60 * 60 * 1000;
 
@@ -14,14 +18,9 @@ export const getToken = () => {
 };
 
 export const setToken = (accessToken: string, refreshToken: string) => {
-  setCookie(TOKEN_KEYS.accessToken, accessToken, {
-    // expires: new Date(Date.now() + ACCESS_EXPIRE_TIME),
-    // TODO
-    // accessToken이 만료될 때 interceptors에서 refresh 재발급 API를 호출합니다.
-    // 그럼에도 불구하고 accessToken을 같이 보내주어야 하는 관계로 expires 시간을 설정하지 않습니다.
-  });
+  setCookie(TOKEN_KEYS.accessToken, accessToken, {});
   setCookie(TOKEN_KEYS.refreshToken, refreshToken, {
-    expires: new Date(Date.now() + REFRESH_EXPIRE_TIME),
+    expires: new Date(jwtDecode<JWTPayload>(refreshToken).exp * 1000),
   });
 };
 
