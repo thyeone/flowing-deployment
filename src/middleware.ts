ac5b841383fc980d6dc4e2d284fa20a7f4241852
thyeone <thyeonee@gmail.com>
2025-01-16 14:07:26 +0000

    fix: 리다이렉션 버그

--- src/middleware.ts
+++ src/middleware.ts
@@ -37,30 +37,30 @@ export async function middleware(req: NextRequest) {
 
   // 프로필 페이지에 접근했을 때 아직 가입절차인 유저일 경우 접근금지
   // 가입 완료한 유저가 join에 접속했을 때
-  if (pathname === '/join') {
-    try {
-      const res = await getMember(accessToken as string, memberId as string);
+  // if (pathname === '/join') {
+  //   try {
+  //     const res = await getMember(accessToken as string, memberId as string);
 
-      if (res.data.status === 'ACTIVE') {
-        return NextResponse.redirect(new URL('/home', req.url));
-      }
-    } catch (error) {
-      return NextResponse.redirect(new URL('/', req.url));
-    }
-  }
+  //     if (res.data.status === 'ACTIVE') {
+  //       return NextResponse.redirect(new URL('/home', req.url));
+  //     }
+  //   } catch (error) {
+  //     return NextResponse.redirect(new URL('/', req.url));
+  //   }
+  // }
 
   // 가입중인 유저가 서비스 내부에 접근했을 경우 가입페이지로 리다이렉션
-  if (pathname !== '/' && !pathname.includes('auth') && !JOIN_PAGES.test(pathname)) {
-    try {
-      const res = await getMember(accessToken as string, memberId as string);
+  // if (pathname !== '/' && !pathname.includes('auth') && !JOIN_PAGES.test(pathname)) {
+  //   try {
+  //     const res = await getMember(accessToken as string, memberId as string);
 
-      if (res.data.status === 'IN_SING_UP') {
-        return NextResponse.redirect(new URL('/join', req.url));
-      }
-    } catch (error) {
-      return NextResponse.redirect(new URL('/', req.url));
-    }
-  }
+  //     if (res.data.status === 'IN_SING_UP') {
+  //       return NextResponse.redirect(new URL('/join', req.url));
+  //     }
+  //   } catch (error) {
+  //     return NextResponse.redirect(new URL('/', req.url));
+  //   }
+  // }
 
   try {
     const tokenPayload = getPayload();
