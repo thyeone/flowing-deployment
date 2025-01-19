2a9cb5c62e52e4eb69693a3c9702b63a377f302d
thyeone <thyeonee@gmail.com>
2025-01-20 00:04:04 +0900

    fix

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,20 @@
-export default function MainLayout({ children }: PropsWithStrictChildren) {
+import { jwtDecode } from 'jwt-decode';
+import { redirect } from 'next/navigation';
+
+import { getCookie } from '@/actions/cookie';
+import { memberApi } from '@/apis/member';
+import { TOKEN_KEYS } from '@/constants';
+
+export default async function MainLayout({ children }: PropsWithStrictChildren) {
+  const accessToken = await getCookie(TOKEN_KEYS.accessToken);
+
+  const decoded: { id: string } = await jwtDecode(accessToken as string);
+
+  const user = await memberApi.getMember(decoded.id);
+
+  if (user.status === 'IN_SING_UP') {
+    redirect('/join');
+  }
+
   return <main className="main-layout">{children}</main>;
 }
