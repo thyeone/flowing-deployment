afe66a099dcc61a101aac235748748c292ea712a
thyeone <thyeonee@gmail.com>
2025-01-19 15:04:43 +0000

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { jwtDecode } from 'jwt-decode';
 import { redirect } from 'next/navigation';
 
 import { getCookie } from '@/actions/cookie';
+import { memberApi } from '@/apis/member';
 import { TOKEN_KEYS } from '@/constants';
 
 import LoginSection from './_components/LoginSection';
@@ -8,7 +10,17 @@ import LoginSection from './_components/LoginSection';
 export default async function LoginPage() {
   const token = await getCookie(TOKEN_KEYS.accessToken);
 
-  if (token) redirect('/home');
+  if (token) {
+    const decoded: { id: string } = jwtDecode(token as string);
+
+    const user = await memberApi.getMember(decoded.id);
+
+    if (token) {
+      if (user.status === 'IN_SING_UP') {
+        redirect('/join');
+      } else redirect('/home');
+    }
+  }
 
   return <LoginSection />;
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,7 @@
 import { jwtDecode } from 'jwt-decode';
 import { NextRequest, NextResponse } from 'next/server';
 
-import commonFetch from './apis/config/fetch';
-import type { MemberResponse } from './apis/member';
 import { TOKEN_KEYS } from './constants';
-
-const JOIN_PAGES = /^\/(join|profile|auth)/;
-
-const getMember = async (accessToken: string, memberId: string) =>
-  await commonFetch<MemberResponse>(`members/${memberId}`, accessToken as string);
 
 export async function middleware(req: NextRequest) {
   const accessToken = req.cookies.get(TOKEN_KEYS.accessToken)?.value;
@@ -22,11 +15,8 @@ export async function middleware(req: NextRequest) {
     return null;
   };
 
-  const memberId = getPayload()?.id;
-
   const { pathname } = req.nextUrl;
 
-  // 예외 경로들에 대해서는 바로 next() 처리
   if (pathname === '/' || pathname.includes('auth')) {
     return NextResponse.next();
   }
@@ -34,33 +24,6 @@ export async function middleware(req: NextRequest) {
   if (!accessToken || !refreshToken) {
     return NextResponse.redirect(new URL('/', req.url));
   }
-
-  // 프로필 페이지에 접근했을 때 아직 가입절차인 유저일 경우 접근금지
-  // 가입 완료한 유저가 join에 접속했을 때
-  // if (pathname === '/join') {
-  //   try {
-  //     const res = await getMember(accessToken as string, memberId as string);
-
-  //     if (res.data.status === 'ACTIVE') {
-  //       return NextResponse.redirect(new URL('/home', req.url));
-  //     }
-  //   } catch (error) {
-  //     return NextResponse.redirect(new URL('/', req.url));
-  //   }
-  // }
-
-  // 가입중인 유저가 서비스 내부에 접근했을 경우 가입페이지로 리다이렉션
-  // if (pathname !== '/' && !pathname.includes('auth') && !JOIN_PAGES.test(pathname)) {
-  //   try {
-  //     const res = await getMember(accessToken as string, memberId as string);
-
-  //     if (res.data.status === 'IN_SING_UP') {
-  //       return NextResponse.redirect(new URL('/join', req.url));
-  //     }
-  //   } catch (error) {
-  //     return NextResponse.redirect(new URL('/', req.url));
-  //   }
-  // }
 
   try {
     const tokenPayload = getPayload();
